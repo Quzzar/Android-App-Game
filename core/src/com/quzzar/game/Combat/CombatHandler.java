0fa598630385a02268f148e94727e1101cf4af04
Curiosity279 <nicolascoffey1@gmail.com>
2018-06-06 23:23:49 -0700

    Update CombatHandler.java

--- core/src/com/quzzar/game/Combat/CombatHandler.java
+++ core/src/com/quzzar/game/Combat/CombatHandler.java
@@ -8,9 +8,7 @@ import com.quzzar.game.Utility;
 import java.util.Random;
 
 public class CombatHandler {
-
     public static void doCombat(Monster enemy){
-
         Stats stats = Player.getStats();
 
         double playerSpeedR = stats.getSpeed()*1.0/enemy.getSpeed();
@@ -18,10 +16,31 @@ public class CombatHandler {
 
         boolean playerHit = checkHit(playerSpeedR);
         boolean enemyHit = checkHit(enemySpeedR);
+        boolean poisonous = enemy.poisonous;
+        boolean poisoned = false;
+        int poisonCount = 0;
+
 
         Utility.print("COMBAT","pSpeed:"+stats.getSpeed());
         Utility.print("COMBAT","eSpeed:"+enemy.getSpeed());
 
+        if (enemyHit && poisonous) {
+            if (poisoned) {
+                poisonCount = 0;
+            } else {
+                poisoned = true;
+                Utility.print("COMBAT", "Player has been poisoned");
+                }
+            }
+        if (poisoned && !playerHit) {
+                Player.hurt(2);
+                Utility.print("COMBAT", "Player took 2 damage from poison" + stats.getHealth());
+                poisonCount++;
+                if (poisonCount == 3) {
+                    poisoned = false;
+                    poisonCount = 0;
+                }
+        }
         if(playerHit){
             enemy.hurt(calculateDamage(stats.getDamage(), enemy.getDefense()));
         }
@@ -37,20 +56,25 @@ public class CombatHandler {
 
     private static double calculateDamage(double attackerDamage, double defenderDefense){
         return attackerDamage - attackerDamage*((defenderDefense*1.0)/20);
+
     }
 
     private static boolean checkHit(double speedR){
+
         int odds = (int) Math.round(1/(0.931238 - 1.034495*Math.pow(Math.E,-0.875008*speedR)));
 
         Random rand = new Random();
 
         int r = rand.nextInt(odds);
 
+
         Utility.print("COMBAT","SpeedR:"+speedR);
 
         Utility.print("COMBAT","Odds:"+odds);
 
         Utility.print("COMBAT","R:"+r);
+
+
 
         return r==0;
     }
